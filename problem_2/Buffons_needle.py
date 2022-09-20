a3d3d6521dbbaf8e7f194cad94a67a378026cdad
junid231 <108266380+junid231@users.noreply.github.com>
2022-09-20 20:37:59 +0900

    Update Buffons_needle.py

--- problem_2/Buffons_needle.py
+++ problem_2/Buffons_needle.py
@@ -1,26 +1,24 @@
 import math, random
 
 def guess_pi():
-    l = input('length of needle: ')
-    d = input('distance between lines: ')
-    if float(l) > float(d):
+    l = float(input('length of needle: '))
+    d = float(input('distance between lines: '))
+    if l > d:
         print('error!\nneedle cannot be longer than distance.')
         return -1
-    print('l: '+l+'\td: '+d)
-    n = input('attempts: ')
+    print('l:', l, '\td:', d)
+    n = int(input('attempts: '))
     tried = 0
-    for i in range(int(n)):
-        x = random.uniform(-float(d)/2, float(d)/2)
+    for i in range(n):
+        x = random.uniform(-d/2, d/2)
         theta = random.uniform(0, math.pi)
-        if abs(x) <= float(l)/2*math.sin(theta):
+        if abs(x) <= l/2*math.sin(theta):
             tried += 1
-        else:
-            continue
     if tried == 0:
         print("Wow... there's no needle cross the line...\ncan't guess pi..")
         return -1
-    p = tried/int(n)
-    guess = 2*float(l)/(p*float(d))
+    p = tried/n
+    guess = 2*l/(p*d)
     print(guess)
 
 guess_pi()
